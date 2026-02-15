fbee8021a903fc8bc9a80d527878c81b5c8536c5
Shirley Solares <gato5.2023d.c@gmail.com>
2026-02-15 00:42:06 -0300

    Create dia2.py

---
+++ dia2.py
@@ -0,0 +1,18 @@
+# Día 2 – Variables y operaciones
+
+nombre = input("¿Cómo te llamas? ")
+edad = int(input("¿Cuántos años tienes? "))
+
+print("Hola", nombre)
+print("El próximo año tendrás", edad + 1, "años")
+
+# Operaciones simples
+suma = 5 + 3
+resta = 10 - 4
+multiplicacion = 6 * 2
+division = 8 / 2
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
